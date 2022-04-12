2c491702a5fc75802d0ebe30d360e54876f6c33b
Marcos8060 <marcosgav80@gmail.com>
2022-04-12 17:45:30 +0300

    created a serializer file and api endpoint views

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from . import views
 
 
 app_name = 'blog_api'
 
 urlpatterns = [
-    # path('',PostList.as_view(),name='listcreate'),
-    # path('<int:pk>',PostDetail.as_view(),name='detailcreate')
+    path('',views.PostList.as_view(),name='listcreate'),
+    path('<int:pk>',views.PostDetail.as_view(),name='detailcreate')
 ]
