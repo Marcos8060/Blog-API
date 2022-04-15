dbb9ea69d696c1837c61212cb919b3ccd53be032
Marcos8060 <marcosgav80@gmail.com>
2022-04-15 11:11:17 +0300

    removed redundant views and replaced with viewsets and their corresponding routers

--- backend/blog_api/urls.py
+++ backend/blog_api/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
-from . import views
+from .views import PostList
+from rest_framework.routers import DefaultRouter
 
 
 app_name = 'blog_api'
 
-urlpatterns = [
-    path('',views.PostList.as_view(),name='listcreate'),
-    path('<int:pk>',views.PostDetail.as_view(),name='detailcreate')
-]
+router = DefaultRouter()
+router.register('', PostList, basename='post')
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('',views.PostList.as_view(),name='listcreate'),
+#     path('<int:pk>',views.PostDetail.as_view(),name='detailcreate')
+# ]
--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from blog.models import *
 from .serializers import *
-from rest_framework.permissions import IsAdminUser,DjangoModelPermissionsOrAnonReadOnly,BasePermission,SAFE_METHODS,AllowAny
+from rest_framework.permissions import IsAdminUser,DjangoModelPermissionsOrAnonReadOnly,BasePermission,SAFE_METHODS,AllowAny,IsAuthenticated
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # only allows the owner of the post to make changes
 class PostUserWritePermission(BasePermission):
@@ -14,14 +17,31 @@ class PostUserWritePermission(BasePermission):
             return obj.author == request.user
 
 
-
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-    queryset = Post.postobjects.all()
-    serializer_class = PostSerializer
+class PostList(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    query_set = Post.postobjects.all()
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
-    permission_classes = [PostUserWritePermission]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    # post list view
+    def list(self,request,):
+        serializer_class = PostSerializer(self.query_set,many=True)
+        return Response(serializer_class.data)
+
+    # post detail view
+    def retrieve(self,request,pk=None):
+        post = get_object_or_404(self.query_set, pk=pk)    
+        serializer_class = PostSerializer(post)
+        return Response(serializer_class.data)
+
+
+
+# class PostList(generics.ListCreateAPIView):
+#     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+#     queryset = Post.postobjects.all()
+#     serializer_class = PostSerializer
+
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
+#     permission_classes = [PostUserWritePermission]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
